756a3243c7fdbd26d70b16742d16f7e930a0ed3c
Terry Lewis <terrylewis@outlook.com>
2014-04-13 12:25:04 -0500

    Using Record for entity Attributes.

--- CoreDataHs.hs
+++ CoreDataHs.hs
@@ -1,9 +1,5 @@
 -- | Main entry point to the application.
 module Main where
-import Data.Char
-import System.Process
-import System.Cmd as S
-import System.Exit
 import Text.XML.Light
 import Data.Maybe
 import Data.List
@@ -14,11 +10,14 @@ data Attribute = Attribute {
                            } deriving Show
 data Entity = Entity {
                         entityName :: (Maybe String)
-                      , entityAttributes :: [(Maybe String)]
+                      , entityAttributes :: [Attribute]
                       , entityRelationships :: [(Maybe String)]
                      } deriving Show
 
 simpleName s = QName s Nothing Nothing
+
+typeAttr :: Element -> (Maybe String)
+typeAttr e = (findAttr $ simpleName "attributeType") e
 
 nameAttr :: Element -> (Maybe String)
 nameAttr e = (findAttr $ simpleName "name") e
@@ -33,7 +32,7 @@ relationships :: Element -> [(Maybe String)]
 relationships e = (map nameAttr (relChild e))
 
 buildEntity :: Element -> Entity
-buildEntity e = (Entity (nameAttr e) (entityAttrs e)  (relationships e))
+buildEntity e = (Entity (nameAttr e) (map buildAttribute (children e))  (relationships e))
 
 entityAttrs :: Element -> [(Maybe String)]
 entityAttrs e = (map nameAttr (children e))
@@ -42,6 +41,9 @@ findEntity :: String -> [Entity] -> (Maybe Entity)
 findEntity "" _ = Nothing
 findEntity _ [] = Nothing
 findEntity s e = find (\(Entity (Just name) _ _) -> name == s) e
+
+buildAttribute :: Element -> Attribute
+buildAttribute e = (Attribute (nameAttr e) (typeAttr e))
 
 main :: IO ()
 
