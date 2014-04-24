502086c19ef9a8ad81c4d550d24cd5f9f9af1cb6
Terry Lewis <terrylewis@outlook.com>
2014-04-23 20:48:13 -0500

    Building full query.

--- QueryBuild.hs
+++ QueryBuild.hs
@@ -14,8 +14,27 @@ operators = [
             ("<=", "IsLessThanOrEqualTo"),
             ("!=", "IsNotEqualTo")
             ]
+
+fetchRequest :: Entity -> String
+fetchRequest e = "\tNSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:" ++ "@\"" ++ (entityName e) ++ "\"];\n"
+
+setPredicate :: Attribute -> String -> String
+setPredicate a s = "\t[fetchRequest setPredicate:[NSPredicate predicateWithFormat:" ++ "@\"" ++ (attrName a) ++  " " ++ s ++ " %@\", " ++ "object]];\n"
+
+sortDescriptor :: String
+sortDescriptor = "\t[fetchRequest setSortDescriptors:sort];\n"
+
+errorVar :: String
+errorVar = "\tNSError *err = nil;\n"
+
+executeFetch :: String
+executeFetch = "\tNSArray *results = [context executeFetchRequest:fetchRequest error:&err];\n"
+
+errorBlock :: String
+errorBlock = "\tif(err && errorBlock) {\n" ++ "\t\terrorBlock(err);\n" ++ "\t\treturn nil;\n" ++ "\t}\n"
+
 signature :: String
-signature = ":(id)object " ++ "inContext:(NSManagedObjectContext *)context " ++ "error:(NSError **)error"
+signature = ":(id)object " ++ "inContext:(NSManagedObjectContext *)context " ++ "sortDescriptors:(NSArray *)sort" ++  " error:(void(^)(NSError *error))errorBlock"
 
 intFileName :: Entity -> String
 intFileName e = (entityName e) ++ "_Fetcher.h"
@@ -40,12 +59,12 @@ buildImplementation e = final
                       where
                       declaration = "@implementation " ++ (entityName e) ++ " (fetch)\n"
 
-                      requests = map buildQueryImplementation (entityAttributes e)
+                      requests = [buildQueryImplementation x y | x <- (entityAttributes e), y <- [e]]
                       end = "@end\n\n"
                       final = declaration ++ (concat requests) ++ end
 
 buildQueryDeclaration :: Attribute -> String
-buildQueryDeclaration a = concat ["- (NSArray *)" ++ y ++ z ++ signature ++ ";\n\n" | (_, z) <- operators, y <- [attrName a]]
+buildQueryDeclaration a = concat ["+ (NSArray *)" ++ y ++ z ++ signature ++ ";\n\n" | (_, z) <- operators, y <- [attrName a]]
 
-buildQueryImplementation :: Attribute -> String
-buildQueryImplementation a = concat ["- (NSArray *)" ++ y ++ z ++ signature ++ " {\n" ++ "\tNSFetchRequest *fetch = [NSFetchRequest fetchRequestWithEntityName:" ++ "@\"" ++ (entName a)  ++ "\"];\n" ++ "}\n" | (x, z) <- operators, y <- [attrName a]]
+buildQueryImplementation :: Attribute -> Entity -> String
+buildQueryImplementation a e = concat ["+ (NSArray *)" ++ y ++ z ++ signature ++ " {\n" ++ (fetchRequest e)  ++ (setPredicate a x) ++ sortDescriptor ++ errorVar ++ executeFetch ++ errorBlock ++ "\treturn results;" ++ "\n}\n\n" | (x, z) <- operators, y <- [attrName a]]
