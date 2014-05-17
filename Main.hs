0e33fd4d676865463654901818d0fca24b5b844e
Terry Lewis <terrylewis@outlook.com>
2014-05-17 12:24:49 -0500

    Small refactoring to Main

--- Main.hs
+++ Main.hs
@@ -6,13 +6,13 @@ import System.Environment
 import Control.Arrow
 
 progressIO :: String -> IO ()
-progressIO p = putStrLn $ "Creating file " ++ p
+progressIO = putStrLn . ("Creating file " ++)
 
 main :: IO ()
 main = do
   args <- getArgs
   let modelName = if not (null args) then head args else error "no Model name given."
-  entities <- readFile (fullModelPath modelName) >>= return . modelEntities
-  sequence_ [(progressIO p) >> writeFile p c | (p, c) <- map (intFileName &&& buildDeclaration) entities]
-  sequence_ [(progressIO p) >> writeFile p c | (p, c) <- map (impFileName &&& buildImplementation) entities]
+  entities <- fmap modelEntities $ readFile $ fullModelPath modelName
+  sequence_ [progressIO p >> writeFile p c | (p, c) <- map (intFileName &&& buildDeclaration) entities]
+  sequence_ [progressIO p >> writeFile p c | (p, c) <- map (impFileName &&& buildImplementation) entities]
   putStrLn "done!"
